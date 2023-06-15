24dec841905cdbe5ff77fcff8e522ea570882a20
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-15 09:28:31 -0400

    add apps

---
+++ epd-back/db/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+
+class Email(models.Model):
+
+    file = models.FileField(_("file"), upload_to='emails/')
+
+    class Meta:
+        verbose_name = _("email")
+        verbose_name_plural = _("emails")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("email_detail", kwargs={"pk": self.pk})
