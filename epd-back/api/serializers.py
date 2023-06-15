b724a2578652bb8c51885ef3ad5df65bbaf547ea
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-15 10:11:45 -0400

    add some fields

--- epd-back/api/serializers.py
+++ epd-back/api/serializers.py
@@ -6,3 +6,4 @@ class EmailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Email
         fields = '__all__'
+        read_only_fields = ['uuid', 'date_created']
--- epd-back/db/models.py
+++ epd-back/db/models.py
@@ -2,14 +2,17 @@ from django.db import models
 from django.urls import reverse
 from django.core.validators import FileExtensionValidator
 from django.utils.translation import gettext_lazy as _
+import uuid
 
 # Create your models here.
 
 
 class Email(models.Model):
 
+    uuid = models.UUIDField(_("uuid"), primary_key=True, default=uuid.uuid4())
     file = models.FileField(_("file"), upload_to='emails/',
                             validators=[FileExtensionValidator(['eml'])])
+    date_created = models.DateTimeField(_("date created"), auto_now=True)
 
     class Meta:
         verbose_name = _("email")
