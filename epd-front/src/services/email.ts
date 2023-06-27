19d4a9a3d66f621d1f6ee9ed5e2880661cd7b324
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-26 23:19:29 -0400

    add email

--- epd-front/src/services/email.ts
+++ epd-front/src/services/email.ts
@@ -14,6 +14,15 @@ export const emailApi = createApi({
     getEmailById: builder.query<Email, string>({
       query: (id) => `emails/${id}`,
     }),
+    addEmail: builder.mutation<Email, Partial<Email>>({
+      query(body) {
+        return {
+          url: `emails`,
+          method: 'POST',
+          body,
+        }
+      },
+    }),
   }),
 })
 
