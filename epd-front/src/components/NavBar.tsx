76da9ab6cbb31791d135325b6c35a3359ec6693b
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-08-05 11:31:39 -0400

    fix style

--- epd-front/src/components/NavBar.tsx
+++ epd-front/src/components/NavBar.tsx
@@ -17,14 +17,14 @@ export function NavBar() {
     <>
       <Box bg={useColorModeValue("green.100", "green.900")} px={4}>
         <Flex h={20} alignItems={"center"} justifyContent={"space-between"}>
-          <Flex gap={4} alignItems={"center"}>
-            <Link to="emails">
+          <Link to="/">
+            <Flex gap={4} alignItems={"center"}>
               <img src={logo} alt="site logo" width={40} height={40} />
               <Text fontSize={24} fontWeight={"bold"}>
                 Email Phishing Detector
               </Text>
-            </Link>
-          </Flex>
+            </Flex>
+          </Link>
 
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={4}>
