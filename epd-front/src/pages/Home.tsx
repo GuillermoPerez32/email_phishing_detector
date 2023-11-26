177b28802768e88e6d9d53b2c0b57628e5432e8c
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-11-26 01:39:28 -0500

    add metrics

--- epd-front/src/pages/Home.tsx
+++ epd-front/src/pages/Home.tsx
@@ -2,6 +2,17 @@ import { Box, Skeleton, Typography } from "@mui/material";
 import { Card } from "../components/Card";
 import { useGetEmailsQuery } from "../services/email";
 
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
 export const Home = () => {
   const { data, isLoading } = useGetEmailsQuery("1");
 
@@ -10,13 +21,51 @@ export const Home = () => {
   const totalNotPhishing =
     data?.filter((email) => email.phishing === "no").length || 0;
 
+  // map data and obtain phishing and not phishing count per day
+  const phishingData = data?.reduce((acc, curr) => {
+    const date = new Date(curr.date_created);
+    const dateStr = `${date.getMonth() + 1}/${date.getDate()}`;
+    const phishing = curr.phishing === "yes" ? 1 : 0;
+    const notPhishing = curr.phishing === "no" ? 1 : 0;
+
+    const existingData = acc.find((data: any) => data.date === dateStr);
+
+    if (existingData) {
+      existingData.phishing += phishing;
+      existingData.notPhishing += notPhishing;
+    } else {
+      acc.push({
+        date: dateStr,
+        phishing,
+        notPhishing,
+      });
+    }
+
+    return acc;
+  }, [] as any);
+
   return (
-    <Card width="100%" height="70%" padding="24px">
-      <Box display="flex" justifyContent="space-evenly">
-        <MetricElement isLoading={isLoading} value={totalPhishing} />
-        <MetricElement isLoading={isLoading} value={totalNotPhishing} />
-      </Box>
-    </Card>
+    <Box>
+      <Card width="100%" padding="24px">
+        <Box display="flex" justifyContent="space-evenly">
+          <MetricElement isLoading={isLoading} value={totalPhishing} />
+          <MetricElement isLoading={isLoading} value={totalNotPhishing} />
+        </Box>
+      </Card>
+      <Card width="100%" padding="24px">
+        <ResponsiveContainer height={300}>
+          <LineChart data={phishingData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Line type="monotone" dataKey="notPhishing" stroke="green" />
+            <Legend />
+            <Line type="monotone" dataKey="phishing" stroke="tomato" />
+          </LineChart>
+        </ResponsiveContainer>
+      </Card>
+    </Box>
   );
 };
 
@@ -33,7 +82,9 @@ export const MetricElement = ({
       {isLoading ? (
         <Skeleton variant="text" />
       ) : (
-        <Typography variant="h4">{value}</Typography>
+        <Typography variant="h2" fontWeight="bold">
+          {value}
+        </Typography>
       )}
     </Box>
   );
